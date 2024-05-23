807edcd5578f825581c98bcc871c15b5c6cdb38a
Miran <112928970+Mcfearless314@users.noreply.github.com>
2024-05-23 15:11:24 +0200

    Added buttons for starting,  reseting and saving runs

--- lib/ui/google_maps.dart
+++ lib/ui/google_maps.dart
@@ -11,8 +11,11 @@ class GoogleMaps extends StatefulWidget {
   _GoogleMapsState createState() => _GoogleMapsState();
 }
 
+enum RunState { notStarted, inProgress, finished }
+
 class _GoogleMapsState extends State<GoogleMaps> {
   GoogleMapController? _mapController;
+  RunState _runState = RunState.notStarted;
   List<LatLng> _routePoints = [];
   Position? _currentPosition;
   final Set<Polyline> _polylines = {};
@@ -54,27 +57,71 @@ class _GoogleMapsState extends State<GoogleMaps> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Run Tracker'),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Theme.of(context).canvasColor,
+        title: Text('Run Tracker', style: TextStyle(fontFamily: 'PoetsenOne', color: Colors.white),
+      )),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(0, 0),
+          zoom: 15,
         ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 15,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+        polylines: _polylines,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _buildFloatingActionButton(),
+    );
+  }
+
+
+  Widget _buildFloatingActionButton() {
+    if (_runState == RunState.notStarted) {
+      return FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _runState = RunState.inProgress;
+          });
+        },
+        child: Icon(Icons.directions_run),
+      );
+    } else if (_runState == RunState.inProgress) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              // Handle save button press
+            },
+            child: Icon(Icons.save),
           ),
-          myLocationEnabled: true,
-          onMapCreated: (GoogleMapController controller) {
-            _mapController = controller;
-          },
-          polylines: _polylines,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.directions_run),
-        ));
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _runState = RunState.finished;
+              });
+            },
+            child: Icon(Icons.refresh),
+          ),
+        ],
+      );
+    } else {
+      return FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _runState = RunState.inProgress;
+          });
+        },
+        child: Icon(Icons.directions_run),
+      );
+    }
   }
 }
+
 
 class Coordinates {
   final double latitude;
@@ -82,3 +129,5 @@ class Coordinates {
 
   Coordinates(this.latitude, this.longitude);
 }
+
+
