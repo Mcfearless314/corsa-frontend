912e31e0de4dab3ab7a9f93e37454aa9785aa5dd
Miran <112928970+Mcfearless314@users.noreply.github.com>
2024-05-23 14:43:30 +0200

    Update saved_run_map.dart

--- lib/ui/saved_run_map.dart
+++ lib/ui/saved_run_map.dart
@@ -15,14 +15,32 @@ class SavedRunMap extends StatefulWidget {
 class _SavedRunMapState extends State<SavedRunMap> {
   GoogleMapController? _mapController;
   final Set<Polyline> _polylines = {};
+  late CameraPosition _initialCameraPosition;
+  final String now = '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}';
 
   @override
   void initState() {
     super.initState();
+    LatLng middleOfRoute = _findMiddleOfRoute();
+    _initialCameraPosition = CameraPosition(
+      target: middleOfRoute,
+      zoom: 16,
+    );
     _showRouteOnMap();
   }
 
+  LatLng _findMiddleOfRoute() {
+    double totalLat = 0;
+    double totalLong = 0;
+    List<Coordinates> route = _getRoute();
 
+    for (Coordinates point in route) {
+      totalLat += point.latitude;
+      totalLong += point.longitude;
+    }
+
+    return LatLng(totalLat / route.length, totalLong / route.length);
+  }
 
   List<Coordinates> _getRoute() {
     return [
@@ -46,7 +64,8 @@ class _SavedRunMapState extends State<SavedRunMap> {
       // Create a new polyline with the points from _getRoute
       _polylines.add(Polyline(
         polylineId: PolylineId('route'),
-        points: _getRoute().map((c) => LatLng(c.latitude, c.longitude)).toList(),
+        points:
+            _getRoute().map((c) => LatLng(c.latitude, c.longitude)).toList(),
         color: Colors.blue,
         width: 5,
       ));
@@ -70,7 +89,7 @@ class _SavedRunMapState extends State<SavedRunMap> {
       );
 
       // Animate the camera to the bounds of the route
-      CameraUpdate cameraUpdate = CameraUpdate.newLatLngBounds(bounds, 50);
+      CameraUpdate cameraUpdate = CameraUpdate.newLatLngBounds(bounds, 10);
       _mapController?.animateCamera(cameraUpdate);
     });
   }
@@ -78,21 +97,20 @@ class _SavedRunMapState extends State<SavedRunMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Run Tracker'),
-        ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(_getRoute().first.latitude, _getRoute().first.longitude),
-            zoom: 16,
-          ),
-          myLocationEnabled: true,
-          onMapCreated: (GoogleMapController controller) {
-            _mapController = controller;
-          },
-          polylines: _polylines,
-        ),
-       );
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text('Run from $now', style: TextStyle(fontFamily: 'PoetsenOne', color: Colors.white)),
+        backgroundColor: Theme.of(context).canvasColor,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+        polylines: _polylines,
+      ),
+    );
   }
 }
 
