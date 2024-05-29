e41867148b945b9214ea3d28b21cad3a60cf7788
Miran <112928970+Mcfearless314@users.noreply.github.com>
2024-05-29 13:19:38 +0200

    added test and a fake BroadcastChannel

--- lib/BroadcastWsChannel.dart
+++ lib/BroadcastWsChannel.dart
@@ -1,14 +1,35 @@
 
 
+import 'dart:async';
+
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class BroadcastWsChannel {
   BroadcastWsChannel(Uri uri): _channel = WebSocketChannel.connect(uri) {
-    stream = _channel.stream.asBroadcastStream();
+    _stream = _channel.stream.asBroadcastStream();
   }
 
   final WebSocketChannel _channel;
-  late Stream<dynamic> stream;
+  late Stream<dynamic> _stream;
+   Stream<dynamic> get stream => _stream;
 
-  WebSocketSink get sink => _channel.sink;
+  StreamSink get sink => _channel.sink;
+}
+
+class FakeBroadcastWsChannel implements BroadcastWsChannel {
+
+  final streamController = StreamController<dynamic>.broadcast();
+  final sinkController = StreamController<dynamic>();
+
+  @override
+  Stream get stream => streamController.stream;
+
+  @override
+  WebSocketChannel get _channel => throw UnimplementedError();
+
+  @override
+  late Stream _stream;
+
+  @override
+  StreamSink get sink => throw UnimplementedError();
 }
--- test/homepage_screen_test.dart
+++ test/homepage_screen_test.dart
@@ -1,25 +1,55 @@
+import 'package:corsa/BroadcastWsChannel.dart';
 import 'package:corsa/ui/homepage_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('HomepageScreen has a title', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: HomePageScreen()));
+    final channel = FakeBroadcastWsChannel();
+    await tester.pumpWidget(MaterialApp(
+      home: Provider<BroadcastWsChannel>.value(
+        value: channel,
+        child: HomePageScreen(),
+      ),
+    ));
     expect(find.text('Welcome to Corsa'), findsOneWidget);
   });
 
-  testWidgets('HomepageScreen has a login button', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: HomePageScreen()));
+  testWidgets('HomepageScreen has a Loginbutton', (WidgetTester tester) async {
+    final channel = FakeBroadcastWsChannel();
+    await tester.pumpWidget(MaterialApp(
+      home: Provider<BroadcastWsChannel>.value(
+        value: channel,
+        child: HomePageScreen(),
+      ),
+    ));
     expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
   });
 
   testWidgets('HomepageScreen has a signup button', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: HomePageScreen()));
-    expect(find.widgetWithText(ElevatedButton, 'Sign up'), findsOneWidget);
-  });
+    final channel = FakeBroadcastWsChannel();
+    await tester.pumpWidget(MaterialApp(
+      home: Provider<BroadcastWsChannel>.value(
+        value: channel,
+        child: HomePageScreen(),
+      ),
+    ));
+        expect(find.widgetWithText(ElevatedButton, 'Sign up'), findsOneWidget);
+      });
 
   testWidgets('HomepageScreen has a username field', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: HomePageScreen()));
-    expect(find.widgetWithText(TextFormField, 'Username'), findsOneWidget);
-  });<
+    final channel = FakeBroadcastWsChannel();
+    await tester.pumpWidget(MaterialApp(
+      home: Provider<BroadcastWsChannel>.value(
+        value: channel,
+        child: HomePageScreen(),
+      ),
+    ));
+        expect(find.widgetWithText(TextFormField, 'Username'), findsOneWidget);
+      });
+
+  /*channel.streamController.add('{"type": "welcome"}');
+  final serverEvent = await channel.sinkController.stream.firstWhere((
+      element) =>)*/
 }
